56f2a223c2895e8ff8d9c5b14af060c13992ba91
krtuhin <tuhin.km2774@gmail.com>
2023-08-27 16:56:42 +0530

    Project Created

---
+++ src/main/java/com/filejsononeapi/FileJsonOneApiApplication.java
@@ -0,0 +1,13 @@
+package com.filejsononeapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileJsonOneApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileJsonOneApiApplication.class, args);
+    }
+
+}
